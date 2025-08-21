871a16e758caba18e1cebdd31fdc77a665475f17
BossLady - shop <mahneyms@gmail.com>
2025-08-21 20:46:55 +0200

    Create src/components/Footer.js
    Add Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-black text-cream p-4 text-center">
+      © 2025 Boss Lady Luxury Shop. All Rights Reserved.
+    </footer>
+  );
+}
